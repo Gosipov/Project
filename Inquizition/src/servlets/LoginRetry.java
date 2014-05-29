c1a4bd1f88b04733a8f1a2f06f5129c1181f46d3
Babdus <Babdusi@gmail.com>
2014-05-29 19:37:21 +0400

    servlets updated

--- Inquizition/src/servlets/LoginRetry.java
+++ Inquizition/src/servlets/LoginRetry.java
@@ -1,11 +1,21 @@
 package servlets;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.DBConnection;
+
 
 /**
  * Servlet implementation class LoginRetry
@@ -26,14 +36,55 @@ public class LoginRetry extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String redirect = "login.jsp";
+		String title1 = "Welcome To Inquizition";
+		String title2 = "Please Log In";
+		request.getServletContext().setAttribute("header1", title1);
+		request.getServletContext().setAttribute("header2", title2);
+		RequestDispatcher dispatch = request.getRequestDispatcher(redirect);
+		dispatch.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try{
+			DBConnection db = (DBConnection) request.getServletContext().getAttribute("database");
+			Statement stat = db.getStatement();
+			String userName = request.getParameter("username");
+			String password = request.getParameter("password");
+			ResultSet result = stat.executeQuery("SELECT password FROM users WHERE name = " + userName);
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			String encryptedPassword = hexToString(md.digest(password.getBytes()));
+			String redirect = "retry.jsp";
+			String title2 = "Username Does Not Exist";
+			if(result.next()){
+				redirect = "welcome.java";
+				String takenPassword = result.getString("password");
+				title2 = "";
+				if(!takenPassword.equals(encryptedPassword)){
+					title2 = "Password Is Incorrect";
+					redirect = "retry.jsp";
+				}
+			}
+			request.getServletContext().setAttribute("title2", title2);
+			RequestDispatcher dispatch = request.getRequestDispatcher(redirect);
+			dispatch.forward(request, response);
+		} catch(SQLException | NoSuchAlgorithmException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
 	}
 
 }
--- Inquizition/src/servlets/SignUpRetry.java
+++ Inquizition/src/servlets/SignUpRetry.java
@@ -1,12 +1,19 @@
 package servlets;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import db.DBConnection;
 
 /**
  * Servlet implementation class SignUpRetry
@@ -34,7 +41,31 @@ public class SignUpRetry extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try{
+			DBConnection db = (DBConnection) request.getServletContext().getAttribute("database");
+			Statement stat = db.getStatement();
+			String userName = request.getParameter("username");
+			String password = request.getParameter("password");
+			MessageDigest md = MessageDigest.getInstance("SHA");
+			String encryptedPassword = hexToString(md.digest(password.getBytes()));
+			stat.executeUpdate("INSERT INTO users (name, password) VALUES "
+					+ "(" + userName + ", " + encryptedPassword + ")");
+			RequestDispatcher dispatch = request.getRequestDispatcher("welcome");
+			dispatch.forward(request, response);
+		} catch(SQLException | NoSuchAlgorithmException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff;  // remove higher bits, sign
+			if (val<16) buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
 	}
 
 }
