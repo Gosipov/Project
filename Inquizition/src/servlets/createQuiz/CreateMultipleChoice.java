03f3f9517b62e2c294c731a4e273478b47b27b97
gosipov <gosip12@freeuni.edu.ge>
2014-06-23 00:03:25 +0400

    script included in CreateMultipleChoice servlet

--- Inquizition/src/servlets/createQuiz/CreateMultipleChoice.java
+++ Inquizition/src/servlets/createQuiz/CreateMultipleChoice.java
@@ -32,27 +32,7 @@ public class CreateMultipleChoice extends HttpServlet {
 		out.print("<br> Enter your answers here and check the right one <br>");
 		out.print("<div id='myList'> </div>");
 		out.print("<button type='button' onclick='myFunction(false)'> Add another option </button>");
-		
-		// adding script, here because of problem with inclusion
-		out.print("<script>"
-				+ "	var x = 0;"
-				+ "function myFunction(c) {"
-					+ "var container = document.getElementById('myList');"
-					+ "var node = document.createElement('input');"
-					+ "node.type = 'radio';"
-					+ "node.name = 'answer';"
-					+ "node.value = x;"
-					+ "node.checked = c;"
-					+ "var text=document.createElement('input');"
-					+ "text.type = 'text';"
-					+ "text.name = 'answer' + x++;"
-					+ "text.id = text.name;"
-					+ "container.appendChild(node);"
-					+ "container.appendChild(text);"
-					+ "container.appendChild(document.createElement('br')); };"
-				+ "myFunction(true);"
-				+ "myFunction(false);"
-			+ "</script>");
+		out.println("<script src='add_options.js'> </script>");
 		out.print("<br><br>");
 		QuizCreationHTML.middlePart(out, "CreateMultipleChoice");
 		QuizCreationHTML.lowerPart(out);
