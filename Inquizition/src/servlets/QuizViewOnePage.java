a702afc2a6736583684a291ae8227a74e93fbf59
Babdus <Babdusi@gmail.com>
2014-06-22 22:37:34 +0400

    QuizViewOnePage started

--- Inquizition/src/servlets/QuizViewOnePage.java
+++ Inquizition/src/servlets/QuizViewOnePage.java
@@ -1,6 +1,11 @@
 package servlets;
 
+import helpers.questions.QuestionHTML;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +24,24 @@ public class QuizViewOnePage extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		String quizName = (String) request.getSession().getAttribute("quiz_name");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("<title>Welcome</title>");
+		out.println("<link rel = \"stylesheet\" type = \"text/css\" href = \"QuizStyle.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>" + quizName + "</h1>");
+		Iterator<QuestionHTML> questions = (Iterator<QuestionHTML>) request.getSession().getAttribute("questions");
+		while(questions.hasNext()){
+			QuestionHTML questionHtml = questions.next();
+			questionHtml.generateHTML(out);
+		}
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 	
