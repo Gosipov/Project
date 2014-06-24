42d8cbc6db56b1a4c230dd533b01ab01710afaef
gosipov <gosip12@freeuni.edu.ge>
2014-06-24 21:58:54 +0400

    Activity addToDB added

--- Inquizition/src/helpers/Activity.java
+++ Inquizition/src/helpers/Activity.java
@@ -3,6 +3,10 @@ package helpers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.mysql.jdbc.Statement;
+
+import db.DBConnection;
+
 public class Activity {
 	
 	private String quiz;
@@ -14,6 +18,20 @@ public class Activity {
 	private String dtime;
 	private int score;
 	
+	private static DBConnection db;
+	
+	public static void setDB(DBConnection connection) {
+		db = connection;
+	}
+	
+	public Activity(int user_id, int quiz_id, int time_elapsed, int score) {
+		this.user_id = user_id;
+		this.quiz_id = quiz_id;
+		this.time_elapsed = time_elapsed;
+		this.score = score;
+	}
+	
+	// retrieving from database
 	public Activity(ResultSet rs) throws SQLException {
 		this.quiz = rs.getString("quizzes.name");
 		this.quiz_id = rs.getInt("quizzes.id");
@@ -23,6 +41,20 @@ public class Activity {
 		this.time_elapsed = rs.getInt("history.time_elapsed");
 		this.dtime = rs.getString("history.tdate");
 		this.score = rs.getInt("history.score");
+	}
+	
+	public void addToDB() {
+		// ???
+		if(db == null) db = new DBConnection();
+		Statement stat = (Statement) db.getStatement();
+		try{
+			stat.executeUpdate("insert into history(user_id, quiz_id, time_elapsed, score) "
+					+ "values(" + user_id + " ," + quiz_id + ", " + time_elapsed + ", " + score + ")");
+		}
+		catch(SQLException ignored){}
+		finally{
+			try { stat.close(); } catch (SQLException e) {}
+		}
 	}
 	
 	public int getUserID() {
@@ -56,5 +88,6 @@ public class Activity {
 	public int getScore(){
 		return score;
 	}
+	
 	
 }
