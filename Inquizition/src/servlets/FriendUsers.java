481a078e876dc06be461d1e1c37de802cd9cda90
wrinkl3 <al.seward@gmail.com>
2014-06-24 21:56:39 +0400

    FriendUsers servlet added

---
+++ Inquizition/src/servlets/FriendUsers.java
@@ -0,0 +1,57 @@
+package servlets;
+
+import helpers.FriendManager;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FriendUsers
+ */
+@WebServlet("/FriendUsers")
+public class FriendUsers extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FriendUsers() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		int u1 = Integer.parseInt(request.getParameter("from_id"));
+		int u2 = Integer.parseInt(request.getParameter("to_id"));
+		String result = "<h3>Something went wrong</h3>";
+		if(FriendManager.makeFriends(u1, u2) && FriendManager.makeFriends(u2, u1)){
+			result = "<strong>User succesfully friended!<strong><br> <a href = \\\"UserPage?other_id='" + u1
+					+ "'\\\">Go to their profile</a>;";
+		}
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Friend Request</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println(result);
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
